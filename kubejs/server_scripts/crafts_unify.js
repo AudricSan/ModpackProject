4b2d7e4a406b749e46a7ed06ebe4f02444f11389
Audric Rosier <audricrosier@gmail.com>
2022-05-05 13:45:47 +0200

    copper unify - fix steel - world update

--- kubejs/server_scripts/crafts_unify.js
+++ kubejs/server_scripts/crafts_unify.js
@@ -29,8 +29,10 @@ onEvent('recipes', e => {
 
     e.remove({ id: 'boss_tools:compressing/compressed_steel' })
 
-    // CREATE
-    // STEEL
+    e.remove({ id: 'tconstruct:smeltery/casting/metal/steel/rod_gold_cast' })
+
+    e.remove({ id: 'tconstruct:smeltery/casting/metal/steel/rod_sand_cast' })
+
     e.stonecutting('boss_tools:steel_block', 'alloyed:steel_block')
     e.stonecutting('immersiveengineering:storage_steel', 'alloyed:steel_block')
 
@@ -49,6 +51,11 @@ onEvent('recipes', e => {
 
     e.shapeless('alloyed:steel_ingot', ['9x kubejs:steel_nugget'])
     e.shapeless('9x kubejs:steel_nugget', 'alloyed:steel_ingot')
+
+    e.smelting('alloyed:steel_ingot', 'kubejs:steel_crushed')
+    e.blasting('alloyed:steel_ingot', 'kubejs:steel_crushed')
+
+    createBulkWashingCrushed('kubejs:steel_crushed', 'kubejs:steel_nugget', 10, 5)
 
     e.recipes.createSequencedAssembly([
         Item.of('boss_tools:compressed_steel').withChance(32.0),
@@ -70,18 +77,155 @@ onEvent('recipes', e => {
 
     createCrushing('kubejs:steel_crushed', 'kubejs:steel_dust', 1, 3, false)
     createMilling('kubejs:steel_crushed', 'kubejs:steel_dust', 1, 3, false)
-    
-    createCrushing('alloyed:steel_ingot', 'kubejs:steel_crushed', 1, 3, 'kubejs:steel_dust')
-    createMilling ('alloyed:steel_ingot', 'kubejs:steel_crushed', 1, 3, false)
 
-	// createMixingItem(input[], output, qte[], heated) 
-    createMixingItem(['kubejs:steel_dust','kubejs:steel_dust'], 'alloyed:steel_ingot', [6,3], 'heated')
-    createMixingItem(['kubejs:steel_dust','kubejs:steel_dust'], 'alloyed:steel_ingot', [3,3], 'superheated')
+    createCrushing('alloyed:steel_ingot', 'kubejs:steel_crushed', 1, 3, 'kubejs:steel_dust')
+    createMilling('alloyed:steel_ingot', 'kubejs:steel_crushed', 1, 3, false)
+
+    // createMixingItem(input[], output, qte[], heated) 
+    createMixingItem(['kubejs:steel_dust', 'kubejs:steel_dust'], 'alloyed:steel_ingot', [6, 3], 'heated')
+    createMixingItem(['kubejs:steel_dust', 'kubejs:steel_dust'], 'alloyed:steel_ingot', [3, 3], 'superheated')
 
     createCutting('boss_tools:compressed_steel', 'kubejs:steel_gear', 5, 20, 'kubejs:steel_dust')
     createPress('kubejs:steel_gear', '2x kubejs:steel_fan')
     createCutting('alloyed:steel_sheet', 'immersiveengineering:stick_steel', 6, 20, 'kubejs:steel_dust')
     createCutting('alloyed:steel_ingot', 'immersiveengineering:stick_steel', 2, 20, 'kubejs:steel_dust')
+
+    //COPPER
+    e.remove({ id: 'immersiveengineering:crafting/ingot_copper_to_nugget_copper' })
+    e.remove({ id: 'tconstruct:common/materials/copper_nugget_from_ingot' })
+    e.remove({ id: 'immersiveengineering:crafting/plate_copper_hammering' })
+
+    e.remove({ id: 'tconstruct:common/materials/copper_bloc_from_ingots' })
+    e.remove({ id: 'immersiveengineering:crafting/ingot_copper_to_storage_copper' })
+
+    e.remove({ id: 'immersiveengineering:crafting/wire_copper' })
+
+    e.remove({ id: 'immersiveengineering:metalpress/wire_copper' })
+    e.remove({ id: 'immersiveengineering:metalpress/rod_copper' })
+
+    e.remove({ id: 'tconstruct:smeltery/casting/metal/copper/wire_gold_cast' })
+    e.remove({ id: 'tconstruct:smeltery/casting/metal/copper/rod_gold_cast' })
+
+    e.remove({ id: 'tconstruct:smeltery/casting/metal/copper/rod_sand_cast' })
+    e.remove({ id: 'tconstruct:smeltery/casting/metal/copper/wire_sand_cast' })
+
+    e.stonecutting('immersiveengineering:storage_copper', 'create:copper_block')
+    e.stonecutting('tconstruct:copper_block', 'create:copper_block')
+
+    replaceIO('immersiveengineering:ingot_copper', 'create:copper_ingot')
+    replaceIO('oreclumps:copper_ingot', 'create:copper_ingot')
+    replaceIO('tconstruct:copper_ingot', 'create:copper_ingot')
+    replaceIO('exnihilosequentia:ingot_copper', 'create:copper_ingot')
+
+    replaceIO('immersiveengineering:plate_copper', 'create:copper_sheet')
+
+    replaceIO('immersiveengineering:nugget_copper', 'create:copper_nugget')
+    replaceIO('tconstruct:copper_nugget', 'create:copper_nugget')
+    replaceIO('immersiveengineering:wire_copper', 'createaddition:copper_wire')
+
+    createCutting('createaddition:copper_rod', 'createaddition:copper_wire', 6, 20, 'kubejs:copper_dust')
+    createCutting('create:copper_ingot', 'createaddition:copper_rod', 3, 20, 'kubejs:copper_dust')
+
+    createCutting('create:copper_sheet', 'createaddition:copper_wire', 4, 10, 'kubejs:copper_dust')
+    createCutting('create:copper_sheet', 'createaddition:copper_rod', 4, 10, 'kubejs:copper_dust')
+
+    createMixingItem(['kubejs:copper_dust', 'kubejs:copper_dust'], 'create:copper_ingot', [6, 3], 'heated')
+    createMixingItem(['kubejs:copper_dust', 'kubejs:copper_dust'], 'create:copper_ingot', [3, 3], 'superheated')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
     function createPress(input, output) {
         e.custom({
@@ -196,104 +340,120 @@ onEvent('recipes', e => {
         }
     }
 
+    function createBulkWashingCrushed(input, output, qte, qte2) {
+        e.custom({
+            "type": "create:splashing",
+
+            "ingredients": [
+                { "item": input }],
+
+            "results": [
+                { "item": output, "count": qte },
+                { "item": output, "count": qte2, "chance": 0.3 }],
+
+            "processingTime": 400
+        })
+    }
+
     //Create Mixing
-	function createMixingItem(input, output, qte, heated) {
-		e.custom({
-			"type": "create:mixing",
+    function createMixingItem(input, output, qte, heated) {
+        e.custom({
+            "type": "create:mixing",
 
-			"ingredients": [
-				{ "item": input[0], "count": qte[0] },
-				{ "item": input[1], "count": qte[1] }
-			],
+            "ingredients": [
+                { "item": input[0], "count": qte[0] },
+                { "item": input[1], "count": qte[1] }
+            ],
 
-			"results": [
-				{ "item": output, 'count': qte[2] }
-			],
+            "results": [
+                { "item": output, 'count': qte[2] }
+            ],
 
-			"processingTime": 400,
-			"heatRequirement": heated
-		})
-	}
+            "processingTime": 400,
+            "heatRequirement": heated
+        })
+    }
 
-	function createMixingItem3(input, output, qte, heated) {
-		e.custom({
-			"type": "create:mixing",
+    function createMixingItem3(input, output, qte, heated) {
+        e.custom({
+            "type": "create:mixing",
 
-			"ingredients": [
-				{ "item": input[0], "count": qte[0] },
-				{ "item": input[1], "count": qte[1] },
-				{ "item": input[2], "count": qte[2] }
-			],
+            "ingredients": [
+                { "item": input[0], "count": qte[0] },
+                { "item": input[1], "count": qte[1] },
+                { "item": input[2], "count": qte[2] }
+            ],
 
-			"results": [
-				{ "item": output, 'count': qte[3] }
-			],
+            "results": [
+                { "item": output, 'count': qte[3] }
+            ],
 
-			"processingTime": 400,
-			"heatRequirement": heated
-		})
-	}
+            "processingTime": 400,
+            "heatRequirement": heated
+        })
+    }
 
-	function createMixingItem4(input, output, qte, heated) {
-		e.custom({
-			"type": "create:mixing",
+    function createMixingItem4(input, output, qte, heated) {
+        e.custom({
+            "type": "create:mixing",
 
-			"ingredients": [
-				{ "item": input[0], "count": qte[0] },
-				{ "item": input[1], "count": qte[1] },
-				{ "item": input[2], "count": qte[2] },
-				{ "item": input[3], "count": qte[3] }
-			],
+            "ingredients": [
+                { "item": input[0], "count": qte[0] },
+                { "item": input[1], "count": qte[1] },
+                { "item": input[2], "count": qte[2] },
+                { "item": input[3], "count": qte[3] }
+            ],
 
-			"results": [
-				{ "item": output, 'count': qte[4] }
-			],
+            "results": [
+                { "item": output, 'count': qte[4] }
+            ],
 
-			"processingTime": 400,
-			"heatRequirement": heated
-		})
-	}
+            "processingTime": 400,
+            "heatRequirement": heated
+        })
+    }
 
-	function createMixingItem5(input, output, qte, heated) {
-		e.custom({
-			"type": "create:mixing",
+    function createMixingItem5(input, output, qte, heated) {
+        e.custom({
+            "type": "create:mixing",
 
-			"ingredients": [
-				{ "item": input[0], "count": qte[0] },
-				{ "item": input[1], "count": qte[1] },
-				{ "item": input[2], "count": qte[2] },
-				{ "item": input[3], "count": qte[3] },
-				{ "item": input[4], "count": qte[4] }
-			],
+            "ingredients": [
+                { "item": input[0], "count": qte[0] },
+                { "item": input[1], "count": qte[1] },
+                { "item": input[2], "count": qte[2] },
+                { "item": input[3], "count": qte[3] },
+                { "item": input[4], "count": qte[4] }
+            ],
 
-			"results": [
-				{ "item": output, 'count': qte[5] }
-			],
+            "results": [
+                { "item": output, 'count': qte[5] }
+            ],
 
-			"processingTime": 400,
-			"heatRequirement": heated
-		})
-	}
+            "processingTime": 400,
+            "heatRequirement": heated
+        })
+    }
 
-	function createMixingItem6(input, output, qte, heated) {
-		e.custom({
-			"type": "create:mixing",
+    function createMixingItem6(input, output, qte, heated) {
+        e.custom({
+            "type": "create:mixing",
 
-			"ingredients": [
-				{ "item": input[0], "count": qte[0] },
-				{ "item": input[1], "count": qte[1] },
-				{ "item": input[2], "count": qte[2] },
-				{ "item": input[3], "count": qte[3] },
-				{ "item": input[4], "count": qte[4] },
-				{ "item": input[5], "count": qte[5] },
-			],
+            "ingredients": [
+                { "item": input[0], "count": qte[0] },
+                { "item": input[1], "count": qte[1] },
+                { "item": input[2], "count": qte[2] },
+                { "item": input[3], "count": qte[3] },
+                { "item": input[4], "count": qte[4] },
+                { "item": input[5], "count": qte[5] },
+            ],
 
-			"results": [
-				{ "item": output, 'count': qte[6] }
-			],
+            "results": [
+                { "item": output, 'count': qte[6] }
+            ],
 
-			"processingTime": 400,
-			"heatRequirement": heated
-		})
-	}
+            "processingTime": 400,
+            "heatRequirement": heated
+        })
+    }
+
 })
--- kubejs/server_scripts/tags_Unify.js
+++ kubejs/server_scripts/tags_Unify.js
@@ -2,31 +2,39 @@ events.listen('item.tags', function (e) {
     //GLOBALS
     // e.remove('forge:sticks'                  , [''])
     // e.remove('forge:rods'                    , [''])
-    e.remove('forge:ingots'                  , ['boss_tools:steel_ingot', 'immersiveengineering:ingot_steel'])  
+    e.remove('forge:ingots'                  , ['boss_tools:steel_ingot', 'immersiveengineering:ingot_steel', 'immersiveengineering:ingot_copper', 'oreclumps:copper_ingot', 'tconstruct:copper_ingot', 'exnihilosequentia:ingot_copper'])  
     e.remove('forge:dusts'                   , ['immersiveengineering:dust_steel'])                                                     
-    e.remove('forge:plates'                  , ['immersiveengineering:plate_steel'])                                                 
-    e.remove('minecraft:beacon_payment_items', ['boss_tools:steel_ingot', 'immersiveengineering:ingot_steel',])                  
-    e.remove('forge:beconpayment'            , ['boss_tools:steel_ingot', 'immersiveengineering:ingot_steel',])      
-    
+    e.remove('forge:plates'                  , ['immersiveengineering:plate_steel', 'immersiveengineering:plate_copper'])                                                 
+    e.remove('minecraft:beacon_payment_items', ['boss_tools:steel_ingot', 'immersiveengineering:ingot_steel', 'immersiveengineering:ingot_copper', 'oreclumps:copper_ingot', 'tconstruct:copper_ingot', 'exnihilosequentia:ingot_copper'])                  
+    e.remove('forge:beconpayment'            , ['boss_tools:steel_ingot', 'immersiveengineering:ingot_steel', 'immersiveengineering:ingot_copper', 'oreclumps:copper_ingot', 'tconstruct:copper_ingot', 'exnihilosequentia:ingot_copper'])      
+    e.remove('forge:nuggets'                 , ['immersiveengineering:nugget_steel','boss_tools:steel_nugget', 'immersiveengineering:nugget_copper', 'tconstruct:copper_nugget'])
+
+    e.add('forge:nuggets'                 , ['kubejs:steel_nugget'])
+    e.add('create:crushed_ores'           , ['kubejs:steel_crushed'])
+    e.add('forge:gears'                   , ['kubejs:steel_gear'])
+    e.add('forge:rods'                    , ['immersiveengineering:stick_steel'])
+    e.add('forge:sticks'                  , ['immersiveengineering:stick_steel'])
+    e.add('forge:beconpayment'            , ['create:copper_ingot'])      
+    e.add('minecraft:beacon_payment_items', ['create:copper_ingot'])                  
+    // e.add('forge:ingots'                  , [])  
+    // e.add('forge:dusts'                   , [])                                                     
+    // e.add('forge:plates'                  , [])                                                 
+
     //Steel
     e.remove('forge:ingots/steel'            , ['boss_tools:steel_ingot', 'immersiveengineering:ingot_steel'])
     e.remove('forge:dusts/steel'             , ['immersiveengineering:dust_steel'])                         
     e.remove('forge:plates/steel'            , ['immersiveengineering:plate_steel'])                         
-   
-    e.remove('forge:nuggets'                 , ['immersiveengineering:nugget_steel','boss_tools:steel_nugget'])
     e.remove('forge:nuggets/steel'           , ['immersiveengineering:nugget_steel', 'boss_tools:steel_nugget'])
     
     e.add('forge:gears/steel'   , 'kubejs:steel_gear')
-    e.add('forge:gears'         , 'kubejs:steel_gear')
-
     e.add('forge:nuggets/steel' , 'kubejs:steel_nugget')
-    e.add('forge:nuggets'       , 'kubejs:steel_nugget')
-
-    e.add('create:crushed_ores' ,'kubejs:steel_crushed')
-
     e.add('forge:rods/all_metal', 'immersiveengineering:stick_steel')
     e.add('forge:rods/steel'    , 'immersiveengineering:stick_steel')
-    e.add('forge:rods'          , 'immersiveengineering:stick_steel')
-    e.add('forge:sticks'        , 'immersiveengineering:stick_steel')
     e.add('forge:sticks/steel'  , 'immersiveengineering:stick_steel')
+
+    //Copper
+
+    e.remove('forge:ingots/copper', ['immersiveengineering:ingot_copper', 'oreclumps:copper_ingot', 'tconstruct:copper_ingot', 'exnihilosequentia:ingot_copper' ])
+    e.remove('forge:nuggets/copper', ['immersiveengineering:nugget_copper', 'tconstruct:copper_nugget'])
+    e.remove('forge:plates/copper', ['immersiveengineering:plate_copper'])
 })
