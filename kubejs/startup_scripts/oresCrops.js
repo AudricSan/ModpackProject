03e95f1379f96d4ec6e30a42d5fb6079f9a0c474
Audric Rosier <audricrosier@gmail.com>
2022-06-18 23:41:06 +0200

    Create oresCrops.js

---
+++ kubejs/startup_scripts/oresCrops.js
@@ -0,0 +1,24 @@
+events.listen('item.registry', function (e) {
+
+    // e.create('ores_dirt').displayName('Ore dirt')
+
+    e.create('craunium_crop').displayName('Craunium Crop')
+    e.create('uranium_crop').displayName('Uranium Crop')
+    e.create('aluminum_crop').displayName('Aluminum Crop')
+    e.create('cobalt_crop').displayName('Cobalt Crop')
+    e.create('zinc_crop').displayName('Zinc Crop')
+    e.create('gold_crop').displayName('Gold Crop')
+    e.create('bronze_crop').displayName('Bronze Crop')
+    e.create('diamond_crop').displayName('Diamond Crop')
+    e.create('electrum_crop').displayName('Electrum Crop')
+    e.create('silicon_crop').displayName('Silicon Crop')
+    e.create('iron_crop').displayName('Iron Crop')
+    e.create('nickel_crop').displayName('Nickel Crop')
+    e.create('silver_crop').displayName('Silver Crop')
+    e.create('lead_crop').displayName('Lead Crop')
+    e.create('copper_crop').displayName('Copper Crop')
+    e.create('steel_crop').displayName('Steel Crop')
+    e.create('desh_crop').displayName('Desh Crop')
+    e.create('tin_crop').displayName('Tin Crop')
+
+})
