bfbd01e6831bf0bfc4084d1fb33ed5998a8542c4
Audric Rosier <audricrosier@gmail.com>
2022-06-21 14:56:13 +0200

    Update craft_BotanyPotsCrops.js

--- kubejs/server_scripts/craft/craft_BotanyPotsCrops.js
+++ kubejs/server_scripts/craft/craft_BotanyPotsCrops.js
@@ -1,19 +1,41 @@
 // REMOVE
-onEvent('recipes', e => {
+onEvent("recipes", e => {
 
-    e.custom({
+    botanypots("kubejs:craunium_crop", ["kubejs:craunium_dust", "kubejs:craunium_crushed"])
+
+    botanypots("kubejs:uranium_crop", ["biggerreactors:yellorium_dust", "create:crushed_uranium_ore"])
+    botanypots("kubejs:aluminum_crop", ["kubejs:aluminum_dust", "create:crushed_aluminum_ore"])
+    botanypots("kubejs:cobalt_crop", ["kubejs:cobalt_dust", "morecreatestuffs:crushed_cobalt_ore"])
+    botanypots("kubejs:zinc_crop", ["kubejs:zinc_dust", "create:crushed_zinc_ore"])
+    botanypots("kubejs:gold_crop", ["kubejs:gold_dust", "create:crushed_gold_ore"])
+    botanypots("kubejs:bronze_crop", ["tconstruct:tinkers_bronze_nugget", "alloyed:bronze_ingot"])
+    botanypots("kubejs:diamond_crop", ["kubejs:diamond_dust", "minecraft:diamond"])
+    botanypots("kubejs:electrum_crop", ["kubejs:electrum_dust", "kubejs:electrum_dust"])
+    botanypots("kubejs:silicon_crop", ["kubejs:silicon_dust", "refinedstorage:silicon"])
+    botanypots("kubejs:iron_crop", ["kubejs:iron_dust", "create:crushed_iron_ore"])
+    botanypots("kubejs:nickel_crop", ["kubejs:nickel_dust", "create:crushed_nickel_ore"])
+    botanypots("kubejs:silver_crop", ["kubejs:silver_dust", "create:crushed_silver_ore"])
+    botanypots("kubejs:lead_crop", ["kubejs:lead_dust", "create:crushed_lead_ore"])
+    botanypots("kubejs:copper_crop", ["kubejs:copper_dust", "create:crushed_copper_ore"])
+    botanypots("kubejs:steel_crop", ["kubejs:steel_dust", "kubejs:steel_crushed"])
+    botanypots("kubejs:desh_crop", ["kubejs:desh_dust", "boss_tools:desh_nugget"])
+    botanypots("kubejs:tin_crop", ["kubejs:tin_dust", "create:crushed_tin_ore"])
+
+    function botanypots(crop, results) {
+
+        e.custom({
             "type": "botanypots:crop",
 
             "seed": {
-                "item": "minecraft:potato"
+                "item": crop
             },
 
             "categories": [
                 "dirt"
             ],
 
-            "growthTicks": 1,
-            
+            "growthTicks": 1000,
+
             "display": {
                 "block": "minecraft:potatoes",
                 "properties": {
@@ -25,7 +47,7 @@ onEvent('recipes', e => {
                 {
                     "chance": 0.75,
                     "output": {
-                        "item": "minecraft:potato"
+                        "item": results[0]
                     },
 
                     "minRolls": 1,
@@ -34,12 +56,13 @@ onEvent('recipes', e => {
                 {
                     "chance": 0.05,
                     "output": {
-                        "item": "minecraft:poisonous_potato"
+                        "item": results[1]
                     },
                     "minRolls": 1,
                     "maxRolls": 2
                 }
             ]
-      })
+        })
 
+    }
 })
