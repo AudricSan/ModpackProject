994e95d09c63dc6ad3acd997dbc543ea09abadd1
Audric Rosier <audricrosier@gmail.com>
2022-06-27 09:29:30 +0200

    Update unify_Steel.js

--- kubejs/server_scripts/unify/unify_Steel.js
+++ kubejs/server_scripts/unify/unify_Steel.js
@@ -11,6 +11,9 @@ onEvent('recipes', e => {
 
     e.remove({ id: 'tconstruct:smeltery/casting/metal/steel/rod_gold_cast' })
     e.remove({ id: 'tconstruct:smeltery/casting/metal/steel/rod_sand_cast' })
+    
+    e.replaceInput({}, 'steampowered:steel_ingot', 'alloyed:steel_ingot');
+    e.replaceOutput({}, 'steampowered:steel_ingot', 'alloyed:steel_ingot');
 
     e.stonecutting('boss_tools:steel_block', 'alloyed:steel_block')
     e.stonecutting('alloyed:steel_block', 'boss_tools:steel_block')
@@ -47,145 +50,145 @@ onEvent('recipes', e => {
     function createPress(input, output) {
         e.custom({
             "type": "create:pressing",
-    
+
             "ingredients": [
                 { "item": input }
             ],
-    
+
             "results": [
                 { "item": output }
             ]
         })
     }
-    
+
     function createCutting(input, output, qte, qte2, secoutput) {
         if (secoutput === false) {
             e.custom({
                 "type": "create:cutting",
-    
+
                 "ingredients": [
                     { "item": input }],
-    
+
                 "results": [
                     { "item": output, "count": qte }],
-    
+
                 "processingTime": 300,
             })
-    
+
         } else {
-    
+
             e.custom({
                 "type": "create:cutting",
-    
+
                 "ingredients": [
                     { "item": input }],
-    
+
                 "results": [
                     { "item": output, "count": qte },
                     { "item": secoutput, "count": qte2, "chance": 0.125 }
                 ],
-    
+
                 "processingTime": 600,
             })
         }
     }
-    
+
     function createCrushing(input, output, qte, qte2, secoutput) {
         if (secoutput === false) {
             e.custom({
                 "type": "create:crushing",
-    
+
                 "ingredients": [
                     { "item": input }
                 ],
-    
+
                 "results": [
                     { "item": output, "count": qte },
                     { "item": output, "count": qte2, "chance": 0.3 },
                 ],
-    
+
                 "processingTime": 400,
             })
         } else {
             e.custom({
                 "type": "create:crushing",
-    
+
                 "ingredients": [
                     { "item": input }
                 ],
-    
+
                 "results": [
                     { "item": output, "count": qte },
                     { "item": output, "count": qte2, "chance": 0.3 },
                     { "item": secoutput, "chance": 0.125 }
                 ],
-    
+
                 "processingTime": 400,
             })
         }
     }
-    
+
     function createMilling(input, output, qte, qte2, secoutput) {
         if (secoutput === false) {
             e.custom({
                 "type": "create:milling",
-    
+
                 "ingredients": [
                     { "item": input }],
-    
+
                 "results": [
                     { "item": output, "count": qte }],
-    
+
                 "processingTime": 400,
             })
-    
+
         } else {
-    
+
             e.custom({
                 "type": "create:milling",
-    
+
                 "ingredients": [
                     { "item": input }],
-    
+
                 "results": [
                     { "item": output, "count": qte },
                     { "item": secoutput, "count": qte2, "chance": 0.125 }
                 ],
-    
+
                 "processingTime": 400,
             })
         }
     }
-    
+
     function createBulkWashingCrushed(input, output, qte, qte2) {
         e.custom({
             "type": "create:splashing",
-    
+
             "ingredients": [
                 { "item": input }],
-    
+
             "results": [
                 { "item": output, "count": qte },
                 { "item": output, "count": qte2, "chance": 0.3 }],
-    
+
             "processingTime": 400
         })
     }
-    
+
     //Create Mixing
     function createMixingItem(input, output, qte, heated) {
         e.custom({
             "type": "create:mixing",
-    
+
             "ingredients": [
                 { "item": input[0], "count": qte[0] },
                 { "item": input[1], "count": qte[1] }
             ],
-    
+
             "results": [
                 { "item": output, 'count': qte[2] }
             ],
-    
+
             "processingTime": 400,
             "heatRequirement": heated
         })
